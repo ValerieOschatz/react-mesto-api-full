cbd709eb49fa75b0373351d7f78f5ae0c3b44e45
Valerie K <valerik210695@gmail.com>
2022-09-14 19:21:41 +0300

    Project 15

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -136,7 +136,7 @@ const login = async (req, res, next) => {
     );
     return res.cookie('jwt', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: NODE_ENV === 'production',
       sameSite: false,
       maxAge: 3600000 * 24 * 7,
     }).send({ token });
